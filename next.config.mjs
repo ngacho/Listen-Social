ee3ace558094f3d90a308f87c418cd2996fc49b2
Nikhil <nikhil.kumar@suprsend.com>
2024-08-04 20:38:54 +0530

    m

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/Listen-Social' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Listen-Social/' : '',
 };
