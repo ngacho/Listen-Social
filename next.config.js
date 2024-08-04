e6d96945cf96185dd649673edb9fe36b5979389b
Nikhil <nikhil.kumar@suprsend.com>
2024-08-04 20:58:05 +0530

    m

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    reactStrictMode: true,
-    basePath: process.env.NODE_ENV === 'production' ? '/Listen-Social' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/Listen-Social' : '',
-  };
-  
