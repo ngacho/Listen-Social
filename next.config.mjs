9c516c32b4557f8692f9734ba255b94b0208750c
Nikhil <nikhil.kumar@suprsend.com>
2024-08-04 20:35:37 +0530

    remove export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/Listen-Social' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Listen-Social/' : '',
 };
