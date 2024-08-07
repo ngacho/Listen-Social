141a390f1fd9e8fafe12cc8a5126fdfd198c8b8d
Nikhil <nikhil.kumar@suprsend.com>
2024-08-07 14:24:08 +0530

    m

--- app/api/redditSearchResults/[userId]/route.js
+++ app/api/redditSearchResults/[userId]/route.js
@@ -1,6 +1,6 @@
 // app/api/redditSearchResults/[userId]/route.js
 import { NextResponse } from 'next/server';
-import clientPromise from '../../../../lib/mongodb';
+import clientPromise from '../../lib/mongodb';
 
 export async function GET(request, { params }) {
   const { userId } = params;
