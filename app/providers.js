18fd6edd5944ae974b2fdd638b8fc050a6afba2c
Nikhil <nikhil.kumar@suprsend.com>
2024-08-08 10:21:07 +0530

    posthog setup

---
+++ app/providers.js
@@ -0,0 +1,21 @@
+// app/providers.js
+'use client'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    capture_pageview: true,
+    capture_pageleave: true, // Enable pageleave capture
+  })
+}
+
+export function CSPostHogProvider({ children }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+}
+
+export function PHProvider({ children }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+}
