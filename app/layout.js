8f4d2dd6e6da2e66cd0c450b35096078839f8e6d
Nikhil <nikhil.kumar@suprsend.com>
2024-08-09 19:33:13 +0530

    added clarity

--- app/layout.js
+++ app/layout.js
@@ -22,6 +22,18 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+      <head>
+          {/* Microsoft Clarity Script */}
+          <Script id="clarity-script" strategy="afterInteractive">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "nkmdsrylks");
+            `}
+          </Script>
+        </head>
         <PHProvider>
           <body className={inter.className}>
             <Script id="vector-script" strategy="afterInteractive">
